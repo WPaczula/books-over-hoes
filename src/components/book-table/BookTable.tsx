a6583c69c512feaf6382c272e9c2154f6d8189fc
WPaczula <wojciech.paczula@gmail.com>
2023-03-31 00:29:17 +0200

    feat: Allow to change the need for review

--- src/components/book-table/BookTable.tsx
+++ src/components/book-table/BookTable.tsx
@@ -1,7 +1,9 @@
 import { type Book } from '@prisma/client'
-import { Checkbox } from 'flowbite-react'
+import { useQueryClient } from '@tanstack/react-query'
+import { getQueryKey } from '@trpc/react-query'
 import React from 'react'
-import { BookStatus } from '~/types/BookStatus'
+import { type BookStatus } from '~/types/BookStatus'
+import { api } from '~/utils/api'
 import BookStatusBadge from '../book-status-badge/BookStatusBadge'
 
 type Props = {
@@ -9,8 +11,19 @@ type Props = {
 }
 
 const Table = ({ data }: Props) => {
+    const queryClient = useQueryClient()
+    const adjustNeedsReviewMutation = api.book.adjustNeedsReview.useMutation({
+        onSettled: async () => {
+            const getBooksQueryKey = getQueryKey(api.book.getPaged)
+            await queryClient.refetchQueries({ queryKey: getBooksQueryKey })
+        }
+    })
+    const handleChangeNeedsReview = (bookId: string, needsReview: boolean) => {
+        adjustNeedsReviewMutation.mutate({ id: bookId, needsReview })
+    }
+
     return (
-        <div className="relative overflow-x-auto">
+        <div className="relative overflow-x-auto w-[85%]">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
@@ -23,7 +36,7 @@ const Table = ({ data }: Props) => {
                         <th scope="col" className="px-6 py-3">
                             Read at
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 w-[200px]">
                             Status
                         </th>
                         <th scope="col" className="px-6 py-3">
@@ -36,7 +49,15 @@ const Table = ({ data }: Props) => {
                         <tr key={book.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <input id="checkbox-table-search-1" type="checkbox" checked={book.needsReview} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <input
+                                        onChange={(event) => {
+                                            handleChangeNeedsReview(book.id, event.target.checked)
+                                        }}
+                                        id="checkbox-table-search-1"
+                                        type="checkbox"
+                                        checked={book.needsReview}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                    />
                                     <label htmlFor="checkbox-table-search-1" className="sr-only">Needs review</label>
                                 </div>
                             </td>
@@ -46,7 +67,7 @@ const Table = ({ data }: Props) => {
                             <td className="px-6 py-4">
                                 {book.readAt?.toDateString() || '-'}
                             </td>
-                            <td className="px-6 py-4">
+                            <td className="px-6 py-4 w-[200px]">
                                 <BookStatusBadge status={book.status as BookStatus} />
                             </td>
                             <td className="px-6 py-4">
--- src/server/api/routers/book.ts
+++ src/server/api/routers/book.ts
@@ -1,17 +1,39 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import {
   createTRPCRouter,
   protectedProcedure,
 } from "~/server/api/trpc";
-import { prisma } from "~/server/db";
 
 export const bookRouter = createTRPCRouter({
   getPaged: protectedProcedure.input(z.object({ page: z.number().min(0), size: z.number().min(10) })).query(async ({input, ctx}) => {
     const userId = ctx.session.user.id
 
-    const books = await prisma.book.findMany({ where: {userId}, take: input.size, skip: input.page * input.size })
+    const books = await ctx.prisma.book.findMany({ where: {userId}, take: input.size, skip: input.page * input.size })
 
     return books
   }),
+
+  adjustNeedsReview: protectedProcedure.input(z.object({ id: z.string().cuid(), needsReview: z.boolean() })).mutation(async ({ctx, input}) => {
+    const userId = ctx.session.user.id
+    const book = await ctx.prisma.book.findUnique({where: {id: input.id}})
+
+    if (!book || book.userId !== userId) {
+        throw new TRPCError({code: 'NOT_FOUND', message: 'Book not found'})
+    }
+
+    book.needsReview = input.needsReview
+
+    if (book.status !== 'TO_BE_READ') {
+        if (book.status === 'COMPLETED' && !book.reviewAdded && input.needsReview) {
+            book.status = 'AWAITING_REVIEW'
+        } else if (book.status === 'AWAITING_REVIEW' && !input.needsReview) {
+            book.status = 'COMPLETED'
+        }
+    }
+
+    await ctx.prisma.book.update({where: {id: input.id}, data: book})
+
+    return book
+  })
 });
